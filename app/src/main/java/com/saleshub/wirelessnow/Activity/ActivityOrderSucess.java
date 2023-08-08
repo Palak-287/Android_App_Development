79acbbbd6b6a35b33fa11a65d9ba2ca35df509b6
kirtikorat <korat.kirti@yogijikrupa.com>
2023-08-08 22:08:51 +0530

    Commit by Kirti Korat 08-08

--- app/src/main/java/com/saleshub/wirelessnow/Activity/ActivityOrderSucess.java
+++ app/src/main/java/com/saleshub/wirelessnow/Activity/ActivityOrderSucess.java
@@ -16,5 +16,10 @@ public class ActivityOrderSucess extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_order_confirmation);
+        finID();
+    }
+
+    private void finID() {
+
     }
 }
